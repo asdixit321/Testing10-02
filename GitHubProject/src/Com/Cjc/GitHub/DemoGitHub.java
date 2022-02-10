0a289d23b28176105a5fc03323348c84b16bef68
asdixit321 <99422332+asdixit321@users.noreply.github.com>
2022-02-11 00:02:42 +0530

    By Roshan

--- GitHubProject/src/Com/Cjc/GitHub/DemoGitHub.java
+++ GitHubProject/src/Com/Cjc/GitHub/DemoGitHub.java
@@ -2,11 +2,16 @@ package Com.Cjc.GitHub;
 
 public class DemoGitHub
 {
+	public void m1()
+	{
+	System.out.println("Hello This is M1");
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("Hello This is Main Method...1");
 		System.out.println("2nd print statement");
 		System.out.println("3rd print Staement");
 		System.out.println("$th Print msg");
+		
 	}
 }
